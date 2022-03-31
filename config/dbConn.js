9b2c06aedaff3e52eeee778349b427a569838c0d
alychinque <alychinque@gmail.com>
2022-03-31 20:27:57 +0100

    Created connection with MongoDB database

---
+++ config/dbConn.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const connectDB = async () => {
+  try {
+    console.log('env: ' + process.env.DATABASE_URI)
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -2,15 +2,21 @@ const { application } = require('express')
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose')
+const connectDB = require('./config/dbConn')
+
 require('dotenv').config()
 
 const PORT = process.env.PORT || 8000
+
+// Connect to MongoDB
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('<h1>It is working</h1>')
 })
 
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+mongoose.connection.once('open', () => {
+  console.log('Connect to MongoDB')
+  app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
 })
