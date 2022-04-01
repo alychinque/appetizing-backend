9444ac0d0cca3864a2b97a9627960de573fb78ff
alychinque <alychinque@gmail.com>
2022-04-01 10:26:26 +0100

    Update Meal model.

--- model/Meal.js
+++ model/Meal.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const mealSchema = new Schema({
-  nameItem: {
+  nameMeal: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
-  priceItem: {
+  priceMeal: {
     type: Number,
     required: true
   },
