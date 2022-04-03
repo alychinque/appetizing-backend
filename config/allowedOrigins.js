b33728be0cbf74c8f52e58eee67915c79d4f9cf2
alychinque <alychinque@gmail.com>
2022-04-03 13:08:41 +0100

    Added angularJS  port

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'https://appetizing.herokuapp.com/',
     'http://127.0.0.1:5500',
-    'http://localhost:3500'
+    'http://localhost:3500',
+    'http://localhost:4200',
 ];
 
 module.exports = allowedOrigins;
