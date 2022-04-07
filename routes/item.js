7389eaf7f1a5ab7be30931177fa20b86ab5d9cd1
Rafaelsousaa <rafael.current@gmail.com>
2022-04-07 12:27:58 +0100

    update and delete route

--- routes/item.js
+++ routes/item.js
@@ -5,5 +5,7 @@ const itemController = require('../controllers/itemController.js');
 router.route('/')
     .post(itemController.createNewItem)
     .get(itemController.getAllItems)
+    .put(itemController.updateItem)
+    .delete(itemController.deleteItem)
 
 module.exports = router;
