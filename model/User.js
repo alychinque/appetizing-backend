72b3f8a10b58fe70096a76e7450a7ad027eb0526
alychinque <alychinque@gmail.com>
2022-04-09 10:45:07 +0100

    Created user model

---
+++ model/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  nameUser: {
+    type: String,
+    required: true
+  },
+  emailUser: {
+    type: String,
+    required: true
+  },
+  phoneUser: {
+    type: String,
+    required: true
+  },
+  passwordUser: {
+    type: Array,
+    required: true
+  },
+  active: {
+    type: Boolean,
+    required: true
+  }
+})
+module.exports = mongoose.model('User', userSchema)
