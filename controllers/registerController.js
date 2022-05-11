5d8e7f6a6fe15ece57a50138dc3278aaf8154a7f
alychinque <alychinque@gmail.com>
2022-05-11 13:00:45 +0100

    Created register and routes

---
+++ controllers/registerController.js
@@ -0,0 +1,26 @@
+const User = require('../model/User.js')
+const bcrypt = require('bcryptjs')
+
+const registerUser = async (req, res, next) => {
+  const user = req.body
+  if ( !req.body.password || Object.keys(req.body.password).length < 6) return res.status(409).json({ 'message': 'password invalid' });
+  const found = await User.findOne({ email: req.body.email }).exec();
+  if (found) return res.status(409).json({ 'message': 'User already exists.' });
+  if (req.body.password != req.body.confirm_password) return res.status(400).json({ 'message': 'Passwords do not match.' });
+  try {
+    //create and store the new user
+    const saltRounds = 10;
+    const hashedPwd = await bcrypt.hash(req.body.password, saltRounds);
+    const result = await User.create({
+      "name": user.name,
+      "email": user.email,
+      "phone": user.phone,
+      "password": hashedPwd,
+      });
+    res.status(201).json({ 'success': `New user ${user.name} created!` });
+  } catch (err) {
+    res.status(400).json({ 'message': err.message });
+  }
+}
+
+module.exports = { registerUser }
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.route('/')
-  .post(userController.createNewUser)
   .get(userController.getAllUsers)
   .put(userController.updateUser)
   .delete(userController.deleteUser)
