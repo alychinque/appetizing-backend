95b775d120775a0fccda2b6d63002f58d42e0b81
alychinque <alychinque@gmail.com>
2022-05-11 16:16:31 +0100

    Added routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
-
+const cookieParser = require('cookie-parser')
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 
@@ -13,16 +13,26 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 9000
 
-// built-in middleware for json 
-app.use(express.json());
-
 // Connect to MongoDB
 connectDB()
 
 // Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
+// built-in middleware to handler urlencoded form data
+app.use(express.urlencoded({ extended: false }))
+
+// built-in middleware for json 
+app.use(express.json());
+
+// middleware for cookies
+app.use(cookieParser())
+
 // Routes
+app.use('/register', require('./routes/register'));
+app.use('/login', require('./routes/login'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
 app.use('/meal', require('./routes/meal'));
 app.use('/item', require('./routes/item'));
 app.use('/allergy', require('./routes/allergy'));
