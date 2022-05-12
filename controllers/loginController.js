f39b32e94ace2b0f6aac7a17574e8c739d6dd526
alychinque <alychinque@gmail.com>
2022-05-12 02:45:58 +0100

    Updated the expire time.

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,8 +4,6 @@ const jwt = require('jsonwebtoken')
 
 
 const handleLogin = async (req, res) => {
-  console.log('chega handleLogin')
-  console.log(req.body.email)
   // checks if they are empty 
   if (!req.body.email || !req.body.password) return res.status(400).json({ 'message': 'Email and password are required.' })
   // fetches in the database if the email is registered
@@ -13,17 +11,13 @@ const handleLogin = async (req, res) => {
   // if not throws a message Unauthorized
   if (!foundUser) return res.sendStatus(401).json({ 'message': 'Unauthorized not foundUser.' });
   // checks if the password entered is equal to the one encrypted
-  console.log(foundUser)
-  console.log(req.body.email)
-  console.log(req.body.password)
   const match = await bcrypt.compare(req.body.password, foundUser.password);
-  console.log(match)
   if (match) {
     // create JWT
     const accessToken = jwt.sign(
       { 'name': foundUser.name },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '30s' }
+      { expiresIn: '5m' }
     )
     const refreshToken = jwt.sign(
       { 'name': foundUser.name },
@@ -33,7 +27,6 @@ const handleLogin = async (req, res) => {
     // Saving refreshToken with current user
     foundUser.refreshToken = refreshToken
     const result = await foundUser.save()
-    console.log(result)
 
 
     res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
