33bd0a292e3bded5f146e4ab43fb6bc4235d143f
alychinque <alychinque@gmail.com>
2022-05-12 02:47:15 +0100

    Removed verifyJWT from login route

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/loginController');
-const verifyJWT = require('../middleware/verifyJWT')
 
 router.route('/')
-  .post(verifyJWT, loginController.handleLogin)
+  .post(loginController.handleLogin)
 
 module.exports = router;
