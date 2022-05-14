f9b447392207eb47ae907e8e7cb688e55990a09b
alychinque <38706688+alychinque@users.noreply.github.com>
2022-05-14 11:34:00 +0100

    test add a # in the domain

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:4200',
-    'https://webusinessapp.com/'
+    'https://webusinessapp.com/#/'
 ];
 
 module.exports = allowedOrigins;
